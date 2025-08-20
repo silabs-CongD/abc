a586499577fae4354c222002e8ecfd38ff85da87
Silabs_CongD <Cong.Do@silabs.com>
2025-08-20 09:56:48 +0700

    cmake prj

---
+++ conanfile.py
@@ -0,0 +1,26 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+
+class HelloConan(ConanFile):
+    name = "hello"
+    version = "1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+    exports_sources = "CMakeLists.txt", "src/*"
+    # win_bash = True
+
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
---
+++ src/hello.cpp
@@ -0,0 +1,6 @@
+#include "hello.h"
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello from Conan package!" << std::endl;
+}
